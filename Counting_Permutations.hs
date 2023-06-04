99c02c1a1cd0195b6aae9d42266838d537b10f5b
DavierSB <daviersanbell@gmail.com>
2023-06-03 22:46:37 -0400

    ya pasa 6 casos de prueba

--- Counting_Permutations.hs
+++ Counting_Permutations.hs
@@ -5,8 +5,10 @@ import Data.Maybe
 main :: IO ()
 main = do
     line <- getLine
-    let (n, k) = splitline line
-    putStr fst(cont_Perm n k empty)
+    let (line1, line2) = splitline line
+        n = read line1 :: Int
+        k = read line2 :: Int
+    putStr (show (fst (cont_Perm n k empty)))
 
 modul :: Integer
 modul = 10^9 + 7
@@ -20,20 +22,24 @@ splitline (x : y : z) | y == ' ' = ([x], z)
                                      in (f ++ [x], s)
 
 easy_calc :: Int -> Int -> Map (Int, Int) Integer -> Integer
-easy_calc n k dict | isJust sum = mod (fromJust sum) modul
+easy_calc n k dict | cond = sumar (fromJust c1) (fromJust c2) n k dict
                    | otherwise = foul
                    where c1 = lookup (n - 1, k) dict
                          c2 = lookup (n, k - 1) dict
-                         c3 = if k >= n then lookup (n - 1, k - n) dict else 0
-                         sum = c1 + c2 - c3 
+                         cond = ((isJust c1) && (isJust c2))
+
+sumar :: Integer -> Integer -> Int -> Int -> Map (Int, Int) Integer -> Integer
+sumar a b n k dict = mod ((mod (a + b - c) modul) + modul) modul
+                     where c = if k >= n then fromJust (lookup (n - 1, k - n) dict) else 0
 
 cont_Perm :: Int -> Int -> Map (Int, Int) Integer -> (Integer, Map (Int, Int) Integer)
-cont_Perm 2 _ _ = 1
-cont_Perm _ 0 _ = 1
+cont_Perm 2 _ dict = (1, dict)
+cont_Perm _ 0 dict = (1, dict)
 cont_Perm n k dict | (value /= foul) = (value, dict)
-                   | otherwise = let (c3, dict_A) = cont_Perm (n - 1) (k - n) dict
+                   | otherwise = let (c3, dict_A) = if k >= n then cont_Perm (n - 1) (k - n) dict else (0, dict)
                                      (c2, dict_B) = cont_Perm n (k - 1) dict_A
                                      (c1, dict_C) = cont_Perm (n - 1) k dict_B
                                      ans = mod (c1 + c2 - c3) modul
-                                   in (ans, insert (n, k) ans dict_C) 
+                                     new_dict = insert (n, k) ans dict
+                                   in (ans, new_dict)
                      where value = easy_calc n k dict
