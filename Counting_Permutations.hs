50e512840556c4b9ec610e56bbad5bf6b5a9d443
DavierSB <daviersanbell@gmail.com>
2023-06-04 22:42:01 -0400

    Counting Permutations (closer)

--- Counting_Permutations.hs
+++ Counting_Permutations.hs
@@ -1,6 +1,7 @@
 import Prelude hiding (lookup)
-import Data.Map
-import Data.Maybe
+import Data.Array
+
+modul = 10^9 + 7
 
 main :: IO ()
 main = do
@@ -8,38 +9,18 @@ main = do
     let (line1, line2) = splitline line
         n = read line1 :: Int
         k = read line2 :: Int
-    putStr (show (fst (cont_Perm n k empty)))
-
-modul :: Integer
-modul = 10^9 + 7
-
-foul :: Integer
-foul = -(modul + 1)
+        mem = create_array (n+1) (k+1)
+    putStr (show (mem!(n, k)))
 
 splitline :: String -> (String, String)
 splitline (x : y : z) | y == ' ' = ([x], z)
                       | otherwise = let (f, s) = splitline(y : z)
                                      in (f ++ [x], s)
 
-easy_calc :: Int -> Int -> Map (Int, Int) Integer -> Integer
-easy_calc n k dict | cond = sumar (fromJust c1) (fromJust c2) n k dict
-                   | otherwise = foul
-                   where c1 = lookup (n - 1, k) dict
-                         c2 = lookup (n, k - 1) dict
-                         cond = ((isJust c1) && (isJust c2))
-
-sumar :: Integer -> Integer -> Int -> Int -> Map (Int, Int) Integer -> Integer
-sumar a b n k dict = mod ((mod (a + b - c) modul) + modul) modul
-                     where c = if k >= n then fromJust (lookup (n - 1, k - n) dict) else 0
-
-cont_Perm :: Int -> Int -> Map (Int, Int) Integer -> (Integer, Map (Int, Int) Integer)
-cont_Perm 2 _ dict = (1, dict)
-cont_Perm _ 0 dict = (1, dict)
-cont_Perm n k dict | (value /= foul) = (value, dict)
-                   | otherwise = let (c3, dict_A) = if k >= n then cont_Perm (n - 1) (k - n) dict else (0, dict)
-                                     (c2, dict_B) = cont_Perm n (k - 1) dict_A
-                                     (c1, dict_C) = cont_Perm (n - 1) k dict_B
-                                     ans = mod (c1 + c2 - c3) modul
-                                     new_dict = insert (n, k) ans dict
-                                   in (ans, new_dict)
-                     where value = easy_calc n k dict
+create_array :: Int -> Int -> Array (Int,Int) Integer
+create_array n k = a where
+                   a = array ((2, 0), (n, k))
+                         ([((2, i), 1) | i <- [0..k]]++
+                          [((i, 0), 1) | i <- [3..n]]++
+                          [((i, j), (mod (a!(i - 1, j) + a!(i, j - 1)) modul) - (if j >= i then a!(i - 1, j - i) else 0)) | (i, j) <- range ((3, 1), (n, k))]
+                        )
